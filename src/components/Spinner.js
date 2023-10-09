4a18e98227956f1016a4c96656afbd988282d3b5
gr1ntch <odewumimighty@gmail.com>
2023-10-10 00:06:34 +0100

    Conditionally changed the color of the spinner on different backgrounds

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,17 +1,21 @@
 import ClipLoader from "react-spinners/ClipLoader";
 
-export default function Spinner() {
+export default function Spinner({checkPageWidth, pageBody}) {
 
   const styles = { 
     position: "fixed",
     top: "50%",
     left: "50%",
     transform: "translate(-50%, -50%)",
-  }
+  };
+
+  const spinnerStyles = {
+    borderColor: checkPageWidth && pageBody.style.backgroundColor === "black" ? "white" : "black", 
+  };
 
   return (
     <div style={styles}>
-      <ClipLoader color="#000" size={50} speedMultiplier={.5}/>
+      <ClipLoader color="#000" cssOverride={spinnerStyles} size={50} speedMultiplier={.5}/>
     </div>
   )
 }
